cdc971eed8f6923abdec0ca208317b35eaffb19d
Valtteri Linna <valinna@edu.lapinamk.fi>
2022-12-02 10:14:28 +0200

    muokattu toimimaan funktioilla

--- Harjoitukset/exercise4_7.py
+++ Harjoitukset/exercise4_7.py
@@ -1,10 +1,9 @@
-text = input("Syötä numero tai teksti:\n")
+text_to_convert = input("Syötä numero tai teksti:\n")
 
-# jos vaihdetaan numero tekstiksi, laskee merkkejen määrän
-# jos merkkejä on alle viisi, vaihtaa numerot tekstiksi, ja tulostaa
-if text.isnumeric():
-    textlengthNum = len(text)
-    if textlengthNum <= 5:
+
+def convert_num_to_text(text):
+    text_length = len(text)
+    if text_length <= 5:
         text = text.replace("1", "yksi ")
         text = text.replace("2", "kaksi ")
         text = text.replace("3", "kolme ")
@@ -19,13 +18,10 @@ if text.isnumeric():
     else:
         print("Liian monta numeroa!")
 
-# jos vaihdetaan teksti numeroiksi, laskee välilyönnit ja lisää yhden
-# tällä saadaan numeroiden määrä
-# jos numeroita on alle viisi, vaihtaa tekstin numeroiksi
-# lopussa poistaa välilyönnit ja tulostaa
-else:
-    textlength = text.count(" ") + 1
-    if textlength <= 5:
+
+def convert_text_to_num(text):
+    text_length = text.count(" ") + 1
+    if text_length <= 5:
         text = text.replace("yksi", "1")
         text = text.replace("kaksi", "2")
         text = text.replace("kolme", "3")
@@ -39,4 +35,17 @@ else:
         text = text.replace(" ", "")
         print(text)
     else:
-        print("Liian monta numeroa!")
+        print("Liian monta sanaa!")
+
+
+# jos vaihdetaan numero tekstiksi, laskee merkkejen määrän
+# jos merkkejä on alle viisi, vaihtaa numerot tekstiksi, ja tulostaa
+if text_to_convert.isnumeric():
+    convert_num_to_text(text_to_convert)
+
+# jos vaihdetaan teksti numeroiksi, laskee välilyönnit ja lisää yhden
+# tällä saadaan numeroiden määrä
+# jos numeroita on alle viisi, vaihtaa tekstin numeroiksi
+# lopussa poistaa välilyönnit ja tulostaa
+else:
+    convert_text_to_num(text_to_convert)
--- Harjoitukset/exercise6_8.py
+++ Harjoitukset/exercise6_8.py
@@ -1,6 +1,33 @@
 import random
 import string
 
+
+def find_password(pass_length):
+    while True:
+        # luo characters muuttujan, joka on merkkijono jossa on kaikki
+        # isot ja pienet kirjaimet, numerot ja special_characters
+        # muuttujassa olevat erikoismerkit
+        characters = string.ascii_letters + string.digits + special_characters
+        # luo password muuttujan joka koostuu satunnaisista characters
+        # muuttujan merkeistä ja joka on käyttäjän antaman length
+        # muuttujan pituinen
+        password = "".join(random.choice(characters) for i in range(pass_length))
+        # katsoo onko vähintään yksi pieni kirjain
+        if sum(map(str.islower, password)) >= 1:
+            # katsoo onko vähintään yksi iso kirjain
+            if sum(map(str.isupper, password)) >= 1:
+                # katsoo onko vähintään yksi numero
+                if sum(map(str.isnumeric, password)) >= 1:
+                    # katsoo onko vähintään yksi merkki special_characters_list listasta
+                    if any(x in password for x in special_characters_list):
+                        # jos kaikki ehdot täyttyvät, ilmoittaa käyttäjälle salasanan
+                        # muuttaa passwordfound muuttujan True:ksi ja poistuu loopista
+                        return password
+                        # jos yksikään ehdoista ei täyty, hyppää takaisin loopin
+                        # alkuun ja generoi uuden satunnaisen password muuttujan
+                        # jatkaa kunnes kaikki ehdot täyttyvät
+
+
 # luo alkuun muuttujan jolla määritellään onko salasana löytynyt
 passwordfound = False
 
@@ -18,31 +45,8 @@ while True:
         # ilmoitetaan siitä ja kysytään uudestaan
         length = int(input("Kuinka pitkän salasanan haluat?\n"))
         if length >= 8:
-            while True:
-                # luo characters muuttujan, joka on merkkijono jossa on kaikki
-                # isot ja pienet kirjaimet, numerot ja special_characters
-                # muuttujassa olevat erikoismerkit
-                characters = string.ascii_letters + string.digits + special_characters
-                # luo password muuttujan joka koostuu satunnaisista characters
-                # muuttujan merkeistä ja joka on käyttäjän antaman length
-                # muuttujan pituinen
-                password = "".join(random.choice(characters) for i in range(length))
-                # katsoo onko vähintään yksi pieni kirjain
-                if sum(map(str.islower, password)) >= 1:
-                    # katsoo onko vähintään yksi iso kirjain
-                    if sum(map(str.isupper, password)) >= 1:
-                        # katsoo onko vähintään yksi numero
-                        if sum(map(str.isnumeric, password)) >= 1:
-                            # katsoo onko vähintään yksi merkki special_characters_list listasta
-                            if any(x in password for x in special_characters_list):
-                                # jos kaikki ehdot täyttyvät, ilmoittaa käyttäjälle salasanan
-                                # muuttaa passwordfound muuttujan True:ksi ja poistuu loopista
-                                print(f"Salasana: {password}")
-                                passwordfound = True
-                                break
-                                # jos yksikään ehdoista ei täyty, hyppää takaisin loopin
-                                # alkuun ja generoi uuden satunnaisen password muuttujan
-                                # jatkaa kunnes kaikki ehdot täyttyvät
+            password = find_password(length)
+            print(f"Salasana: {password}")
         else:
             print("Liian lyhyt salasana.")
             continue
